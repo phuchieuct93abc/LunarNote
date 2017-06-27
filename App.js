978f963d442aa3c3bee71318bd099dac419ce1c3
phuchieuct <phuchieuct93@gmail.com>
2017-06-27 22:17:48 +0700

    Update

--- App.js
+++ App.js
@@ -1,23 +1,14 @@
 import React from 'react';
+import HomePage from './pages/homepage';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{padding:20}}> 
+        <HomePage></HomePage>
+
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
