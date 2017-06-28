fd61fcdc6e32081c88973a7db7b0b9bfe8a82ab6
hieu <phuchieuct93@gmail.com>
2017-06-28 18:47:06 +0700

    dasd

--- App.js
+++ App.js
@@ -1,14 +1,78 @@
 import React from 'react';
-import HomePage from './pages/homepage';
-import { StyleSheet, Text, View } from 'react-native';
+import List from './pages/list'
+import {  Text, View ,Button,FlatList} from 'react-native';
 
-export default class App extends React.Component {
+import {  StackNavigator} from 'react-navigation';
+
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+   title: 'Welcome',
+ }; 
+
+  constructor(props){
+      super(props);
+      this.state={
+        list:[]
+      }
+
+
+  }
+
   render() {
-    return (
-      <View style={{padding:20}}> 
-        <HomePage></HomePage>
+    const { navigate } = this.props.navigation;
 
+    let buttons = [
+      {
+        name:"Tin mới",
+        key:"url1"
+      },
+      {
+        name:"Công nghệ",
+        key:"url2"
+      },    {
+            name:"Tin mới",
+            key:"url3"
+          },
+          {
+            name:"Tin mới",
+            key:"url4"
+          },
+          {
+            name:"Công nghệ",
+            key:"url5"
+          },    {
+                name:"Tin mới",
+                key:"url6"
+              },
+
+    ]
+
+    return (
+      <View style={{flex:1,padding:20}}>
+          <View>
+            <Text>Header</Text>
+          </View>
+
+          <View style={{flexDirection: 'column',flex:1,flexWrap: 'wrap' }}>
+
+                  {buttons.map((item) => {return <Button style={{height:30}} title={item.name} onPress={()=>navigate("List")}></Button>})}
+
+          </View>
+
+          <View>
+            <Text>Footer</Text>
+          </View>
       </View>
+
+
     );
   }
 }
+
+
+const App = StackNavigator({
+  Home: { screen: HomeScreen },
+  List:{screen:List}
+
+});
