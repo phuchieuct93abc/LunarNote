bce40ca08efd11fa001024521db556b319322e4a
phuchieuct <phuchieuct93@gmail.com>
2017-07-04 22:14:10 +0700

    Update

--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -8,38 +8,65 @@ import {
   View,
   Button,
   FlatList,
-  Dimensions,Text
+  Dimensions,Text,StyleSheet,TouchableOpacity
 } from "react-native";
 import VideoPlayer from "react-native-video-controls";
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 export default class CustomVideoPlayer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { loadVideo: false };
+    this.state = { loadVideo: false,loading:true };
+  }
+  _onPressButton(){
+     this.setState({loadVideo:true})
+    console.log(123)
   }
 
   render() {
     const screenWidth = Dimensions.get("window").width - 20;
     let posterUrl = this.props.poster;
     let videoUrl = this.props.video;
-    if (this.state.loadVideo == true) {
+    const height = 200;
+
+    const styles = StyleSheet.create({
+      playButtonWrapper: {
+        position:"absolute",
+        top:40,
+        flex:1,
+        width:screenWidth,
+        height:height+10,
+        alignItems:"center",
+        justifyContent:"center",
+        backgroundColor:"rgba(128, 128, 128, 0.47)",
+
+      }
+     
+    });
+
+    if (this.state.loadVideo) {
+    
       return (
         <VideoPlayer
           source={{ uri: videoUrl }} // Can be a URL or a local file.
-          paused={true}
           style={{ flex: 1, width: screenWidth, height: 150 }}
         />
       );
     } else {
       return (
-        <View style={{flex:1}}>
-        <Text>Video : {posterUrl}</Text>
+        <View style={{flex:1,alignItems:"center",width: screenWidth, height: 250}}>
+            <Text>Video : {posterUrl}</Text>
           
            <Image
             source={{ uri: posterUrl }}
-            style={{ flex: 1, width: screenWidth ,height:200}}
+            style={{ width: screenWidth ,height:200}}
           />
+            <TouchableOpacity onPress={this._onPressButton.bind(this)} style={styles.playButtonWrapper}>
+            <Icon name="rocket" size={30} color="#900"  style={{ alignItems:"center"}} />
+
+
+          </TouchableOpacity>
         </View>
        
         )
