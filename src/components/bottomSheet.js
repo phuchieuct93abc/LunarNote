d74febbc794bc174aea1a2aed70c358a99e7d6d0
phuchieuct <phuchieuct93@gmail.com>
2017-07-16 16:18:08 +0700

    Update

--- src/components/bottomSheet.js
+++ src/components/bottomSheet.js
@@ -11,98 +11,103 @@ import {
   Dimensions,
   StyleSheet,
   Slider,
-  Switch,Linking,Picker
+  Switch,
+  Linking,
+  Picker
 } from "react-native";
 import { Card, Text } from "react-native-elements";
 import { connect } from "react-redux";
-import {changeNightMode,changeFontSize} from "../actions"
+import { changeNightMode, changeFontSize } from "../actions";
 class BottomSheet extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      finish: false
-    };
-  }
-  openSource(){
-    Linking.openURL(this.props.selectedArticle.ContentUrl)
+
+  openSource() {
+    Linking.openURL(this.props.selectedArticle.ContentUrl);
   }
 
   render() {
-
     return (
-      <View style={[styles.flex,styles.wrapper,{flexDirection:"column"}]}>
-        <View style={{flex:1,flexDirection:"row"}}>
-
+      <View style={[styles.flex, styles.wrapper, { flexDirection: "column" }]}>
+        <View style={{ flex: 1, flexDirection: "row" }}>
           <View style={[styles.flex]}>
-            <View style={[styles.flexColumn,,{alignItems:"center",justifyContent:"flex-start"}]}>
-              <Text style={{flex:0}}>Font size:</Text>
+            <View
+              style={[
+                styles.flexColumn,
+                ,
+                { alignItems: "center", justifyContent: "flex-start" }
+              ]}
+            >
+              <Text style={{ flex: 0 }}>Font size:</Text>
 
-              <Picker style={styles.flex}
+              <Picker
+                style={styles.flex}
                 selectedValue={this.props.fontSize}
                 onValueChange={this.props.changeFontSize}
-                >
+              >
                 <Picker.Item label="Small" value={1} />
-                <Picker.Item label="Medium" value={2}/>
-                <Picker.Item label="Large" value={3}/>
+                <Picker.Item label="Medium" value={2} />
+                <Picker.Item label="Large" value={3} />
               </Picker>
             </View>
-
           </View>
           <View style={styles.flex}>
-            <View style={[styles.flexColumn,,{alignItems:"center",justifyContent:"flex-start"}]}>
-              <Text style={{flex:0}}>Night mode:</Text>
-              <Switch  value={this.props.isNightMode} onValueChange={this.props.changeNightMode}/>
+            <View
+              style={[
+                styles.flexColumn,
+                ,
+                { alignItems: "center", justifyContent: "flex-start" }
+              ]}
+            >
+              <Text style={{ flex: 0 }}>Night mode:</Text>
+              <Switch
+                value={this.props.isNightMode}
+                onValueChange={this.props.changeNightMode}
+              />
             </View>
-
           </View>
-
         </View>
 
-
-
-
-
-        <View style={{flex:1,justifyContent:"center"}}>
-          <Button onPress={this.openSource.bind(this)} title="Open original source"></Button>
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <Button
+            onPress={this.openSource.bind(this)}
+            title="Open original source"
+          />
         </View>
-
-
       </View>
     );
   }
 }
 const mapStateToProps = state => {
-  let selectedArticle = state.article[state.values.selectedArticleIndex]
+  let selectedArticle = state.article[state.values.selectedArticleIndex];
   return {
     isNightMode: state.values.nightMode,
-    selectedArticle:selectedArticle,
-    fontSize:parseInt(state.values.fontSize)
+    selectedArticle: selectedArticle,
+    fontSize: parseInt(state.values.fontSize)
   };
 };
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-  changeNightMode:(value)=>{
-    dispatch(changeNightMode())
-  },
-  changeFontSize:(value)=>{
-    dispatch(changeFontSize(value))
-
-  }
-
+    changeNightMode: value => {
+      dispatch(changeNightMode());
+    },
+    changeFontSize: value => {
+      dispatch(changeFontSize(value));
+    }
   };
 };
-const BottomSheetScreen = connect(mapStateToProps, mapDispatchToProps)(BottomSheet);
+const BottomSheetScreen = connect(mapStateToProps, mapDispatchToProps)(
+  BottomSheet
+);
 export default BottomSheetScreen;
 
- const styles = StyleSheet.create({
-   flexColumn:{
-     flex:1,
-     flexDirection:"row"
-   },
-   flex:{
-     flex:1
-   },
-   wrapper:{
-     padding:10
-   }
+const styles = StyleSheet.create({
+  flexColumn: {
+    flex: 1,
+    flexDirection: "row"
+  },
+  flex: {
+    flex: 1
+  },
+  wrapper: {
+    padding: 10
+  }
 });
